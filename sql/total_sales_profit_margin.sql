49ff1d203db70076aae0418ca49acb9844901a82
Mike Covelli <196670706+theSpacePope91@users.noreply.github.com>
2026-03-07 14:50:48 -0500

    Add SQL query for total sales and profit margin

---
+++ sql/total_sales_profit_margin.sql
@@ -0,0 +1,6 @@
+-- What is our overall sales and profit performance?
+SELECT 
+    ROUND(SUM(Sales), 2) AS Total_Sales,
+    ROUND(SUM(Profit), 2) AS Total_Profit,
+    ROUND((SUM(Profit) / SUM(Sales)) * 100, 2) AS Profit__Margin
+FROM sample___superstore
